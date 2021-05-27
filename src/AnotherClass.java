9970040667b2e736f96a5d1dbaf40f304161fd77
cbastani <36131395+cbastani@users.noreply.github.com>
2021-05-27 12:20:26 +0200

    erster Test mit Idea

---
+++ src/AnotherClass.java
@@ -0,0 +1,9 @@
+public class AnotherClass {
+    public static int myMethod(int i) {
+        if (i >= 42) {
+            return (i * 6);
+        } else {
+            return (i * 7);
+        }
+    }
+}
---
+++ src/CbMain.java
@@ -0,0 +1,10 @@
+import java.security.spec.RSAOtherPrimeInfo;
+
+public class CbMain {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Noch was");
+        System.out.println(
+                AnotherClass.myMethod(6));
+    }
+}
